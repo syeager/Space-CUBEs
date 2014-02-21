f529ee5cfe0eaf8775f8b0eab93bb1b606f59110
Steve Yeager <steve.f.yeager@gmail.com>
2014-02-20 16:48:06 -0800

    Public method to color vertices.

---
+++ Space CUBEs Project/Assets/Code/Editor/Inspector/ColorVerticesEditor.cs
@@ -0,0 +1,28 @@
+﻿// Steve Yeager
+// 2.19.2014
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// 
+/// </summary>
+[CustomEditor(typeof(ColorVertices))]
+public class ColorVerticesEditor : Editor
+{
+    #region 
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Color"))
+        {
+            ColorVertices cv = target as ColorVertices;
+            cv.Bake();
+        }
+    }
+    
+    #endregion
+}
