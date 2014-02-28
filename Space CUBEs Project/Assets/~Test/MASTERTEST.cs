8aaf3df379729202397cb116c16d10acd35960e4
Steve Yeager <steve.f.yeager@gmail.com>
2014-02-28 07:30:05 -0800

    Tweaking.
    - Deleted old CUBE Models.

--- Space CUBEs Project/Assets/~Test/MASTERTEST.cs
+++ Space CUBEs Project/Assets/~Test/MASTERTEST.cs
@@ -7,17 +7,5 @@ using System.Collections;
 //
 public class MASTERTEST : MonoBehaviour
 {
-    public float angle;
 
-
-
-    void Start()
-    {
-        {
-            Debug.Log("Sin\n0: " + Utility.SinZero(0) + "\n90: " + Utility.SinZero(90) + "\n180: " + Utility.SinZero(180) + "\n270: " + Utility.SinZero(270) + "\n360: " + Utility.SinZero(360));
-        }
-        {
-            Debug.Log("Cos\n0: " + Utility.CosZero(0) + "\n90: " + Utility.CosZero(90) + "\n180: " + Utility.CosZero(180) + "\n270: " + Utility.CosZero(270) + "\n360: " + Utility.CosZero(360));
-        }
-    }
 }
