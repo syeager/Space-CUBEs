93c396852feb3ab0018a36befeecf3782537698b
Steve Yeager <steve.f.yeager@gmail.com>
2014-03-04 16:33:36 -0800

    Parallax foundation.

---
+++ Space CUBEs Project/Assets/Code/Scenery/BackgroundObject.cs
@@ -0,0 +1,47 @@
+﻿// Steve Yeager
+// 3.3.2014
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// </summary>
+public class BackgroundObject : MonoBase
+{
+    #region Private Fields
+
+    private float speed = 0f;
+    private Transform myTransform;
+
+    #endregion
+
+    
+    public void Initialize(Transform parent, Vector3 position, float scale, float speed)
+    {
+        this.speed = speed;
+        myTransform.localScale = Vector3.one*scale;
+        myTransform.Rotate(Vector3.forward, Random.Range(0f, 360f));
+        myTransform.parent = parent;
+        myTransform.position = position;
+    }
+
+
+    private void Awake()
+    {
+        myTransform = transform;
+    }
+
+
+    private void Update()
+    {
+        myTransform.position += Vector3.left*speed*deltaTime;
+    }
+
+
+    private void OnBecomeInvisible()
+    {
+        Debug.Log("gone");
+        Destroy(gameObject);
+    }
+}
---
+++ Space CUBEs Project/Assets/Code/Scenery/Background_Manager.cs
@@ -0,0 +1,70 @@
+﻿// Steve Yeager
+// 3.3.2014
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// </summary>
+public class Background_Manager : MonoBase
+{
+    #region Public Fields
+    
+    public float backgroundSpeed;
+    public float start;
+    public float vertical;
+    public float[] positions;
+    public float[] speeds;
+    public float[] minDelays;
+    public float[] maxDelays;
+    public float[] minSizes;
+    public float[] maxSizes;
+    public GameObject[] objects;
+    
+    #endregion
+
+    #region Private Fields
+
+    private Material material;
+    private Transform myTransform;
+
+    #endregion
+
+
+    #region MonoBehaviour Overrides
+
+    private void Awake()
+    {
+        myTransform = transform;
+        material = renderer.sharedMaterial;
+        material.mainTextureOffset = Vector2.zero;
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            StartCoroutine(Spawn(i));
+        }
+    }
+
+
+    private void Update()
+    {
+        // background
+        material.mainTextureOffset += Vector2.right * backgroundSpeed * deltaTime;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private IEnumerator Spawn(int layer)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minDelays[layer], maxDelays[layer]));
+            (Instantiate(objects[Random.Range(0, objects.Length)]) as GameObject).GetComponent<BackgroundObject>().Initialize(myTransform, new Vector3(start, Random.Range(-vertical, vertical), positions[layer]), Random.Range(minSizes[layer], maxSizes[layer]), speeds[layer]);
+        }
+    }
+
+    #endregion
+}
