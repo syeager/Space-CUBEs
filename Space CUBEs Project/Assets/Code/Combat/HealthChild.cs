90c01f168acd013bcbbbeeab5b9d7f78f1527b46
Steve Yeager <steve.f.yeager@gmail.com>
2014-03-27 20:14:21 -0700

    Tweaking.

---
+++ Space CUBEs Project/Assets/Code/Combat/HealthChild.cs
@@ -0,0 +1,27 @@
+﻿// Steve Yeager
+// 3.25.2014
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Send hit information to parent.
+/// </summary>
+public class HealthChild : Health
+{
+    #region Public Fields
+    
+    public Health parent;
+    
+    #endregion
+
+
+    #region MonoBehaviour Overrides
+
+    public override void RecieveHit(Ship sender, HitInfo hitInfo)
+    {
+        parent.RecieveHit(sender, hitInfo);
+    }
+
+    #endregion
+}
