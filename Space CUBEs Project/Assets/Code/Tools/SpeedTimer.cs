61ef979e54e956199dc230ef0a918b57815b5620
Steve Yeager <steve.f.yeager@gmail.com>
2014-04-02 23:41:00 -0700

    SpeedTimer.
    - Closes #238
    - Moved tools.

---
+++ Space CUBEs Project/Assets/Code/Tools/SpeedTimer.cs
@@ -0,0 +1,40 @@
+﻿// Steve Yeager
+// 4.2.2014
+
+using UnityEngine;
+using System;
+using System.Diagnostics;
+
+/// <summary>
+/// Uses a Stopwatch to time code execution.
+/// </summary>
+public class SpeedTimer : IDisposable
+{
+    #region Private Fields
+
+    private readonly string message;
+    private readonly Stopwatch stopwatch;
+
+    #endregion
+
+
+    #region Constructors
+
+    public SpeedTimer(string message)
+    {
+        this.message = message;
+        stopwatch = Stopwatch.StartNew();
+    }
+
+    #endregion
+
+    #region IDisposable Overrides
+
+    public void Dispose()
+    {
+        stopwatch.Stop();
+        UnityEngine.Debug.Log(string.Format("Profiled {0}: {1:0.00}ms", message, stopwatch.ElapsedMilliseconds));
+    }
+
+    #endregion
+}
