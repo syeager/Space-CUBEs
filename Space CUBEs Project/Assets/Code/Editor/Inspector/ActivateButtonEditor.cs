58b522e828264ded89350f38722a01035110727b
Steve Yeager <steve.f.yeager@gmail.com>
2014-04-03 22:55:21 -0700

    Editor for Activate Buttons.

---
+++ Space CUBEs Project/Assets/Code/Editor/Inspector/ActivateButtonEditor.cs
@@ -0,0 +1,39 @@
+﻿// Steve Yeager
+// 4.3.2014
+
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+[CustomEditor(typeof(ActivateButton))]
+public class ActivateButtonEditor : UIButtonEditor
+{
+    #region Editor Overrides
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        ActivateButton button = (target as ActivateButton);
+        bool toggled = false;
+        EditorGUI.BeginChangeCheck();
+        {
+            toggled = EditorGUILayout.Toggle("Enabled", button.isEnabled);
+        }
+        if (EditorGUI.EndChangeCheck())
+        {
+            button.isEnabled = toggled;
+            button.UpdateColor(toggled, true);
+        }
+
+        base.OnInspectorGUI();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("value"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    #endregion
+}
