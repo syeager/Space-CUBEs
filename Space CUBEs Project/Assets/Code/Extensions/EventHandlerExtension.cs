3d5ce58c57fa78794a301a584c6246a2490bde45
Steve Yeager <steve.f.yeager@gmail.com>
2014-04-06 17:55:39 -0700

    Extension for checking null events.

---
+++ Space CUBEs Project/Assets/Code/Extensions/EventHandlerExtension.cs
@@ -0,0 +1,23 @@
+﻿// Steve Yeager
+// 4.6.2014
+// Found online.
+
+using System;
+
+/// <summary>
+/// Extension methods for EventHandlers.
+/// </summary>
+public static class EventHandlerExtension
+{
+    #region Extension Methods
+    
+    /// <summary>
+    /// Send event if it is not empty.
+    /// </summary>
+    public static void Fire<T>(this EventHandler<T> self, object sender, T args) where T : EventArgs
+    {
+        if (self != null) self(sender, args);
+    }
+    
+    #endregion
+}
