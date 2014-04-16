62a0c2f1e9eeb839e2cee9535b5b9e92897f414d
Steve Yeager <steve.f.yeager@gmail.com>
2014-04-15 17:25:23 -0700

    Guard AI foundation.
    Closes #246

---
+++ Space CUBEs Project/Assets/Code/Combat/Enemy/GuardLaser.cs
@@ -0,0 +1,34 @@
+﻿// Steve Yeager
+// 4.14.2014
+
+using UnityEngine;
+
+/// <summary>
+/// Laser shot by Guard enemy.
+/// </summary>
+public class GuardLaser : Weapon
+{
+    #region Public Fields
+
+    public GameObject laser;
+    public float damage;
+    
+    #endregion
+
+    #region Weapon Overrides
+
+    public override void Activate(bool pressed, float multiplier)
+    {
+        if (pressed)
+        {
+            laser.SetActive(true);
+            laser.GetComponent<Hitbox>().Initialize(myShip, damage);
+        }
+        else
+        {
+            laser.SetActive(false);
+        }
+    }
+
+    #endregion
+}
---
+++ Space CUBEs Project/Assets/Code/Controllers/Guard.cs
@@ -0,0 +1,106 @@
+﻿// Steve Yeager
+// 4.13.2014
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// AI for Guard. Stand back and fire straight lasers.
+/// </summary>
+public class Guard : Enemy
+{
+    #region State Fields
+
+    private const string SpawningState = "Spawning";
+    private const string MovingState = "Moving";
+    private const string AttackingState = "Attacking";
+    
+    #endregion
+
+    #region Attacking Fields
+
+    /// <summary>Seconds in between attacks.</summary>
+    public float attackBuffer;
+
+    /// <summary>How long the attack lasts in seconds.</summary>
+    public float attackTime;
+
+    #endregion
+
+
+    #region MonoBehaviour Overrides
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // states
+        stateMachine = new StateMachine(this, SpawningState);
+        stateMachine.CreateState(SpawningState, SpawnEnter, info => { });
+        stateMachine.CreateState(MovingState, info => stateMachine.SetUpdate(MovingUpdate()), info => { });
+        stateMachine.CreateState(AttackingState, info => stateMachine.SetUpdate(AttackingUpdate()), info => { });
+        stateMachine.CreateState(DyingState, DyingEnter, info => { });
+    }
+
+    #endregion
+
+    #region State Methods
+
+    private void SpawnEnter(Dictionary<string, object> info)
+    {
+        myHealth.Initialize();
+
+        // initialize path from level manager
+        path = (Path)info["path"];
+        path.Initialize(myTransform);
+
+        stateMachine.SetState(MovingState);
+    }
+
+
+    private IEnumerator MovingUpdate()
+    {
+        while (true)
+        {
+            Vector3 direction = path.Direction(deltaTime);
+
+            // reached destination
+            if (direction == Vector3.zero)
+            {
+                stateMachine.SetState(AttackingState);
+                yield break;
+            }
+            else
+            {
+                myMotor.Move(direction);
+            }
+
+            yield return null;
+        }
+    }
+
+
+    private IEnumerator AttackingUpdate()
+    {
+        WaitForSeconds buffer = new WaitForSeconds(attackBuffer);
+        WaitForSeconds attack = new WaitForSeconds(attackTime);
+
+        while (true)
+        {
+            yield return buffer;
+            myWeapons.Activate(0, true);
+            yield return attack;
+            myWeapons.Activate(0, false);
+        }
+    }
+
+
+    private void DyingEnter(Dictionary<string, object> info)
+    {
+        StopAllCoroutines();
+        poolObject.Disable();
+    }
+
+    #endregion
+}
