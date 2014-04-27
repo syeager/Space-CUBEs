342dd0a9c2a5f185eb0ef30f67ec924e60bbfaee
Steve Yeager <steve.f.yeager@gmail.com>
2014-04-27 14:06:45 -0700

    Moved keyboard shortcuts to its own file.

---
+++ Space CUBEs Project/Assets/Code/Tools/KeyboardCommands.cs
@@ -0,0 +1,33 @@
+﻿// Steve Yeager
+// 4.26.2014
+
+using System;
+using UnityEngine;
+using Annotations;
+
+/// <summary>
+/// Script to put keyboard shortcuts that aid in debugging.
+/// </summary>
+public class KeyboardCommands : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [UsedImplicitly]
+    private void Update()
+    {
+        // reload level
+        if (Input.GetKeyUp(KeyCode.Keypad9))
+        {
+            GameData.ReloadLevel();
+        }
+
+        // clear console
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetAssembly(typeof(UnityEditor.SceneView));
+            Type type = assembly.GetType("UnityEditorInternal.LogEntries");
+            System.Reflection.MethodInfo method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
+        }
+    }
+#endif
+}
