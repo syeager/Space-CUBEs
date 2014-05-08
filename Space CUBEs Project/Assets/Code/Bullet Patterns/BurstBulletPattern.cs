39b1ceabe4fafb356851684ed14e7f92a094a261
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-07 23:35:28 -0700

    Can set a starting angle.

--- Space CUBEs Project/Assets/Code/Bullet Patterns/BurstBulletPattern.cs
+++ Space CUBEs Project/Assets/Code/Bullet Patterns/BurstBulletPattern.cs
@@ -15,6 +15,7 @@ public class BurstBulletPattern : Weapon
     public float speed;
     public float damage;
     public Vector3 offset;
+    public float startAngle;
     
     #endregion
 
@@ -35,12 +36,12 @@ public class BurstBulletPattern : Weapon
 
     private void Fire()
     {
-        float angle = 360f/number;
+        float angle = 360f / number;
         Vector3 position = myTransform.position + myTransform.TransformDirection(offset);
 
         for (int i = 0; i < number; i++)
         {
-            Vector3 rotation = Utility.RotateVector(Vector3.left, angle*i, Vector3.back);
+            Vector3 rotation = Utility.RotateVector(Vector3.left, angle*i + startAngle, Vector3.back);
             PoolManager.Pop(Bullet_Prefab, position, Quaternion.LookRotation(rotation, Vector3.back)).
                 GetComponent<Hitbox>().Initialize(myShip, damage, rotation*speed);
         }
