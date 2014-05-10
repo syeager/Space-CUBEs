cf78176a4b21cec45ccada0d4f84f265856cc0aa
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-09 19:00:27 -0700

    Print method that doesn't require Singleton.

--- Space CUBEs Project/Assets/Code/Debug/Debugger.cs
+++ Space CUBEs Project/Assets/Code/Debug/Debugger.cs
@@ -193,6 +193,18 @@ public class Debugger : Singleton<Debugger>
     #region Static Methods
 
     /// <summary>
+    /// Displays a log message to the console. Doesn't use Singleton.
+    /// </summary>
+    /// <param name="message">Message to be displayed.</param>
+    /// <param name="context">Unity Object to highlight in the Hierarchy.</param>
+    [System.Diagnostics.Conditional("DEBUG")]
+    public static void Print(object message, Object context = null)
+    {
+        Debug.Log(message, context);
+    }
+
+
+    /// <summary>
     /// Displays a log message to the Console.
     /// </summary>
     /// <param name="message">Message to be displayed.</param>
