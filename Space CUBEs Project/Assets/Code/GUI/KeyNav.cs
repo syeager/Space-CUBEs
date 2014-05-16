351dafce9e3c31acf4e041a4c06d3a9062ca07e1
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-15 17:51:49 -0700

    Disables selected on start up on mobile.

---
+++ Space CUBEs Project/Assets/Code/GUI/KeyNav.cs
@@ -0,0 +1,34 @@
+﻿// Steve Yeager
+// 
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 
+/// </summary>
+public class KeyNav : UIKeyNavigation
+{
+    #region MonoBehaviour Overrides
+
+    protected override void OnEnable()
+    {
+        list.Add(this);
+
+#if UNITY_STANDALONE
+        if (startsSelected)
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying) return;
+#endif
+            if (UICamera.selectedObject == null || !NGUITools.GetActive(UICamera.selectedObject))
+            {
+                UICamera.currentScheme = UICamera.ControlScheme.Controller;
+                UICamera.selectedObject = gameObject;
+            }
+        }
+#endif
+    }
+
+    #endregion
+}
