fba7e5f3ca490e96976a3ec5e594bee23c470449
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-16 18:25:59 -0700

    Optional params.

--- Space CUBEs Project/Assets/Code/Extensions/EventHandlerExtension.cs
+++ Space CUBEs Project/Assets/Code/Extensions/EventHandlerExtension.cs
@@ -16,16 +16,19 @@ public static class EventHandlerExtension
     /// </summary>
     public static void Fire<T>(this EventHandler<T> self, object sender, T args) where T : EventArgs
     {
-        if (self != null) self(sender, args);
+        if (self != null)
+        {
+            self(sender, args);
+        }
     }
 
 
     /// <summary>
     /// Send event if it is not empty.
     /// </summary>
-    public static void Fire(this EventHandler self, object sender, EventArgs args)
+    public static void Fire(this EventHandler self, object sender = null, EventArgs args = null)
     {
-        if (self != null) self(sender, args);
+        if (self != null) self(sender, args ?? EventArgs.Empty);
     }
     
     #endregion
