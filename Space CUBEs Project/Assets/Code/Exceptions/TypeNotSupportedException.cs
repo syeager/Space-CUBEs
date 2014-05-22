3bc23a2dc2c480e63a90968df2d7f08238665247
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-21 22:26:32 -0700

    Exception for a type not supported.

---
+++ Space CUBEs Project/Assets/Code/Exceptions/TypeNotSupportedException.cs
@@ -0,0 +1,38 @@
+﻿// Steve Yeager
+// 5.20.2014
+
+using System;
+
+
+/// <summary>
+/// Exception for when a certain function doesn't support the passed type.
+/// </summary>
+public class TypeNotSupportedException : NotSupportedException
+{
+    #region Readonly Fields
+
+    public readonly object objectNotSupported;
+    public readonly string message;
+
+    #endregion
+
+    #region Exception Overrides
+
+    public override string Message
+    {
+        get { return message; }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public TypeNotSupportedException(object objectNotSupported, string message = "")
+        : base(message)
+    {
+        this.objectNotSupported = objectNotSupported;
+        this.message = string.Format("Type: \"{0}\" not supported. {1}", objectNotSupported.GetType().Name, message);
+    }
+
+    #endregion
+}
