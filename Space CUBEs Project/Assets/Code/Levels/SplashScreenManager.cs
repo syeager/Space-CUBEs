459d6dbb8cd4e2028b174d540037fe3cc3181279
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-28 20:21:29 -0700

    Placeholder Splash Screen.

---
+++ Space CUBEs Project/Assets/Code/Levels/SplashScreenManager.cs
@@ -0,0 +1,115 @@
+﻿// Space CUBEs Project-csharp
+// Author: Steve Yeager
+// Created: 2014.05.28
+// Edited: 2014.05.28
+
+using Annotations;
+using UnityEngine;
+
+/// <summary>
+/// Manages the Dev and Game Splash Screens.
+/// </summary>
+public class SplashScreenManager : Singleton<SplashScreenManager>
+{
+    #region Public Fields
+
+    /// <summary>Time in seconds to display Dev Splash.</summary>
+    public float devTime;
+
+    /// <summary>Time in seconds to display Game Splash.</summary>
+    public float gameTime;
+
+    #endregion
+
+    #region Private Fields
+
+    /// <summary>Splash screens needed to be shown.</summary>
+    private enum States
+    {
+        Dev,
+        Game
+    }
+
+    /// <summary>Current splash being shown.</summary>
+    private States state;
+
+    #endregion
+
+    #region Dev Fields
+
+    /// <summary>GUI title for Little Byte.</summary>
+    public GameObject devTitle;
+
+    #endregion
+
+    #region Game Fields
+
+    /// <summary>GUI title for the game.</summary>
+    public GameObject gameTitle;
+
+    #endregion
+
+    #region MonoBehaviour Overrides
+
+    [UsedImplicitly]
+    private void Start()
+    {
+        Invoke("ShowDevSplash", 1f);
+    }
+
+
+    [UsedImplicitly]
+    private void Update()
+    {
+        bool skip;
+
+#if UNITY_STANDALONE
+        skip = Input.anyKeyDown;
+#else
+        skip = Input.GetKeyDown(KeyCode.Escape) || (Input.touchCount > 0 && Input.GetTouch(0).tapCount > 0);
+#endif
+
+        if (skip)
+        {
+            if (state == States.Dev)
+            {
+                ShowGameSplash();
+            }
+            else
+            {
+                LoadMainMenu();
+            }
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    [UsedImplicitly]
+    private void ShowDevSplash()
+    {
+        state = States.Dev;
+        devTitle.SetActive(true);
+        Invoke("ShowGameSplash", devTime);
+    }
+
+
+    private void ShowGameSplash()
+    {
+        CancelInvoke();
+        state = States.Game;
+        devTitle.SetActive(false);
+        gameTitle.SetActive(true);
+        Invoke("LoadMainMenu", gameTime);
+    }
+
+
+    private void LoadMainMenu()
+    {
+        CancelInvoke();
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    #endregion
+}
