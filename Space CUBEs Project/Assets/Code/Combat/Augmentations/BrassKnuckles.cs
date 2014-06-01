f704d99319197cfe547bb82ad5395a232a9d7857
Steve Yeager <steve.f.yeager@gmail.com>
2014-05-31 17:11:43 -0700

    Brass Knuckles change collider layer.

--- Space CUBEs Project/Assets/Code/Combat/Augmentations/BrassKnuckles.cs
+++ Space CUBEs Project/Assets/Code/Combat/Augmentations/BrassKnuckles.cs
@@ -1,5 +1,7 @@
-﻿// Steve Yeager
-// 5.3.2014
+﻿// Space CUBEs Project-csharp
+// Author: Steve Yeager
+// Created: 2014.05.03
+// Edited: 2014.05.31
 
 using Annotations;
 using UnityEngine;
@@ -9,10 +11,27 @@ using UnityEngine;
 /// </summary>
 public class BrassKnuckles : Augmentation
 {
+    #region References
+
+    private GameObject myGameObject;
+    private Collider myCollider;
+
+    #endregion
+
     #region Public Fields
 
     public float damage = 10f;
-    
+
+    #endregion
+
+    #region Private Fields
+
+    /// <summary>Physics layer for PlayerShip.</summary>
+    private int shipLayer;
+
+    /// <summary>Physics layer for PlayerWeapon.</summary>
+    private int weaponLayer;
+
     #endregion
 
     #region Private Fields
@@ -23,19 +42,23 @@ public class BrassKnuckles : Augmentation
 
     #endregion
 
-
     #region Augmentation Overrides
 
     public override void Initialize(Player player)
     {
         ship = player;
         player.BarrelRollEvent += OnBarrelRoll;
+
+        myGameObject = gameObject;
+        myCollider = collider;
+        shipLayer = LayerMask.NameToLayer("PlayerShip");
+        weaponLayer = LayerMask.NameToLayer("PlayerWeapon");
     }
 
 
     public override Augmentation Bake(GameObject player)
     {
-        BrassKnuckles comp = (BrassKnuckles)player.AddComponent(typeof(BrassKnuckles));
+        var comp = (BrassKnuckles)player.AddComponent(typeof(BrassKnuckles));
         comp.index = index;
         comp.damage = damage;
 
@@ -51,7 +74,7 @@ public class BrassKnuckles : Augmentation
     {
         if (!dealingDamage) return;
 
-        Health enemyHealth = (Health)other.GetComponent(typeof(Health));
+        var enemyHealth = other.GetComponent(typeof(Health)) as Health;
         if (enemyHealth != null)
         {
             enemyHealth.RecieveHit(ship, damage * multiplier);
@@ -64,16 +87,20 @@ public class BrassKnuckles : Augmentation
 
     public void OnBarrelRoll(object sender, ValueArgs args)
     {
-        bool rolling = (bool)args.value;
+        var rolling = (bool)args.value;
         if (rolling)
         {
             dealingDamage = true;
-            collider.enabled = true;
+            myCollider.isTrigger = true;
+            myCollider.enabled = true;
+            myGameObject.layer = weaponLayer;
             multiplier = ship.myWeapons.damageMultiplier;
         }
         else
         {
             dealingDamage = false;
+            myCollider.isTrigger = false;
+            myGameObject.layer = shipLayer;
         }
     }
 
