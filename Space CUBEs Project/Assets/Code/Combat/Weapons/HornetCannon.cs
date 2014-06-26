d158ccbdf0b1b5200b1c6b32cbc860a5ce42b88a
Steve Yeager <steve.f.yeager@gmail.com>
2014-06-25 17:22:50 -0700

    Hornet base.

---
+++ Space CUBEs Project/Assets/Code/Combat/Weapons/HornetCannon.cs
@@ -0,0 +1,76 @@
+﻿// Space CUBEs Project-csharp
+// Author: Steve Yeager
+// Created: 2014.06.25
+// Edited: 2014.06.25
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Fires lasers in quick succession at the player.
+/// </summary>
+public class HornetCannon : Weapon
+{
+    #region Public Fields
+
+    /// <summary>Laser prefab.</summary>
+    public PoolObject laserPrefab;
+
+    /// <summary>Local displacement for firing laser.</summary>
+    public Vector3 cannon;
+
+    /// <summary>Number of times to fire.</summary>
+    public int fireCount = 3;
+
+    /// <summary>Delay in seconds before firing startings.</summary>
+    public float activateDelay;
+
+    /// <summary>Delay in seconds after each shot.</summary>
+    public float fireDelay;
+
+    /// <summary>Damage each shot does.</summary>
+    public float damage;
+
+    /// <summary>Speed in m/s to fire lasers.</summary>
+    public float laserSpeed;
+
+    #endregion
+
+    #region Weapon Overrides
+
+    public override Coroutine Activate(bool pressed, float multiplier, object attackInfo = null)
+    {
+        if (pressed)
+        {
+            return StartCoroutine(Fire());
+        }
+        else
+        {
+            StopAllCoroutines();
+            return null;
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private IEnumerator Fire()
+    {
+        Transform player = LevelManager.Main.playerTransform;
+        if (player == null) yield break;
+        Vector3 direction = (player.position - myTransform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.back);
+
+        yield return new WaitForSeconds(activateDelay);
+
+        WaitForSeconds delay = new WaitForSeconds(fireDelay);
+        for (int i = 0; i < fireCount; i++)
+        {
+            Prefabs.Pop(laserPrefab, myTransform.position + myTransform.TransformDirection(cannon), targetRotation).GetComponent<Hitbox>().Initialize(myShip, damage, direction * laserSpeed);
+            yield return delay;
+        }
+    }
+
+    #endregion
+}
---
+++ Space CUBEs Project/Assets/Code/Controllers/Hornet.cs
@@ -0,0 +1,108 @@
+﻿// Space CUBEs Project-csharp
+// Author: Steve Yeager
+// Created: 2014.06.22
+// Edited: 2014.06.23
+
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Pops in, takes a couple shot at the player, then leaves.
+/// </summary>
+public class Hornet : Enemy
+{
+    #region State Names
+
+    private const string SpawningState = "Spawning";
+    private const string EnteringState = "Entering";
+    private const string AttackingState = "Attacking";
+    private const string ExitingState = "Exiting";
+
+    #endregion
+
+    #region Attacking Fields
+
+    /// <summary>Time in seconds to sit and attack.</summary>
+    public float attackingTime = 3f;
+
+    #endregion
+
+    #region MonoBehaviour Overrides
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // states
+        stateMachine = new StateMachine(this, SpawningState);
+        stateMachine.CreateState(SpawningState, SpawningEnter, info => { });
+        stateMachine.CreateState(EnteringState, info => stateMachine.SetUpdate(EnteringUpdate()), info => { });
+        stateMachine.CreateState(AttackingState, info => stateMachine.SetUpdate(AttackingUpdate()), info => { });
+        stateMachine.CreateState(ExitingState, info => stateMachine.SetUpdate(ExitingUpdate()), info => { });
+        stateMachine.CreateState(DyingState, DyingEnter, info => { });
+    }
+
+    #endregion
+
+    #region State Methods
+
+    private void SpawningEnter(Dictionary<string, object> info)
+    {
+        path = (Path)info["path"];
+        myHealth.Initialize();
+        path.Initialize(myTransform);
+
+        stateMachine.SetState(EnteringState);
+    }
+
+
+    private IEnumerator EnteringUpdate()
+    {
+        while (true)
+        {
+            Vector2 direction = path.Direction(deltaTime);
+
+            // reached destination
+            if (direction == Vector2.zero)
+            {
+                stateMachine.SetState(AttackingState);
+                yield break;
+            }
+            else
+            {
+                myMotor.Move(direction);
+            }
+
+            yield return null;
+        }
+    }
+
+
+    private IEnumerator AttackingUpdate()
+    {
+        yield return myWeapons.Activate(0, true);
+        myWeapons.Activate(0, false);
+        stateMachine.SetState(ExitingState);
+    }
+
+
+    private IEnumerator ExitingUpdate()
+    {
+        while (true)
+        {
+            myMotor.Move(path.Direction(deltaTime));
+            yield return null;
+        }
+    }
+
+
+    private void DyingEnter(Dictionary<string, object> info)
+    {
+        StopAllCoroutines();
+        myWeapons.Activate(0, false);
+        poolObject.Disable();
+    }
+
+    #endregion
+}
