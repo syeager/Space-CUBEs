e2a41fff8da181fb33553b7d7aba53e35068c474
Steve Yeager <steve.f.yeager@gmail.com>
2014-10-19 17:51:22 -0700

    Find all usages in scene

---
+++ Space CUBEs Project/Assets/Little Byte/Scripts/Unity Extensions/Editor/FindReferences.cs
@@ -0,0 +1,19 @@
+﻿// Little Byte Games
+// Author: Steve Yeager
+// Created: 2014.10.19
+// Edited: 2014.10.19
+
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class FindReferences
+{
+    [MenuItem("CONTEXT/MonoBehaviour/Find In Scene")]
+    public static void FindReferencesInScene(MenuCommand command)
+    {
+        MonoBehaviour script = (MonoBehaviour)command.context;
+
+        Selection.objects = Resources.FindObjectsOfTypeAll(script.GetType()).Where(obj => !AssetDatabase.Contains(obj)).ToArray();
+    }
+}
