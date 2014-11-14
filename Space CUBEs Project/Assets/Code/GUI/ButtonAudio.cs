c278c41b5c97344e03aec335e2f0d87b0c2333f2
Steve Yeager <steve.f.yeager@gmail.com>
2014-11-13 21:20:49 -0800

    GUI Audio base

---
+++ Space CUBEs Project/Assets/Code/GUI/ButtonAudio.cs
@@ -0,0 +1,16 @@
+﻿// Little Byte Games
+
+using UnityEngine;
+
+namespace SpaceCUBEs
+{
+    public class ButtonAudio : MonoBehaviour
+    {
+        public GUIAudio.GUIType guiType;
+
+        public void OnClick()
+        {
+            GUIAudio.Main.Play(guiType);
+        }
+    }
+}
---
+++ Space CUBEs Project/Assets/Code/GUI/GUIAudio.cs
@@ -0,0 +1,47 @@
+﻿// Little Byte Games
+
+using Annotations;
+using LittleByte.Audio;
+using UnityEngine;
+
+namespace SpaceCUBEs
+{
+    public class GUIAudio : Singleton<GUIAudio>
+    {
+        #region References
+
+        [SerializeField, UsedImplicitly]
+        private Transform myTransform;
+
+        #endregion
+
+        #region Public Fields
+
+        public enum GUIType
+        {
+            BasicButton,
+            Cancel,
+            Save,
+            Confirm,
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        [SerializeField, UsedImplicitly]
+        private AudioPlayer[] players;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Play(GUIType guiType)
+        {
+            AudioPlayer player = AudioManager.Play(players[(int)guiType]);
+            player.transform.parent = myTransform;
+        }
+
+        #endregion
+    }
+}
